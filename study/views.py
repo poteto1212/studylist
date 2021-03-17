498e816127056bf4cf96d284d2f7c206b9d6314c
Your Name <you@example.com>
2021-03-17 13:27:10 +0000

    init

--- study/views.py
+++ study/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views import generic
 from .models import Studylist
 from .models import SubSubjectList
+from .models import SubjectList
 
 class Study(generic.TemplateView):
     template_name='study/study.html'
@@ -11,4 +12,7 @@ class Study(generic.TemplateView):
 class StudylistView(generic.ListView):
     template_name='study/problemlist.html'
     model=Studylist
+   
     
+        
+  
